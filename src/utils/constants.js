5e232ea37b1f5a487611d5fa8f4019faa0d8af95
Bhavesh Kumbhare <bhaveshkumbhare18@gmail.com>
2026-02-05 14:24:32 +0530

    fix: remove quotes from env and cleanup config

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,4 @@ const config = {
   capacity: 30,
 };
 
-console.log("API Key Check:", import.meta.env.VITE_FIREBASE_API_KEY);
-console.log("URL Check:", import.meta.env.VITE_FIREBASE_BASE_URL);
-
 export default config;
